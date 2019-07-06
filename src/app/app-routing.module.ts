0ce1d92e20b0c8f95a8da152bd864145658e83ed
kevinJV <163199@ids.upchias.edu.mx>
2019-07-06 15:09:41 -0500

    Pages create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,42 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#OnboardingPageModule' },
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
+  { path: 'tips', loadChildren: './tips/tips.module#TipsPageModule' },
+  { path: 'events', loadChildren: './events/events.module#EventsPageModule' },
+  { path: 'create-event', loadChildren: './create-event/create-event.module#CreateEventPageModule' },
+
+   /*{ path: '', component: HomePage, children:[
+        { path: 'news', children: [
+              {
+                path: 'news',
+                loadChildren: '../news/news.module#NewsPageModule'
+              }
+            ]
+        },
+        {
+          path: 'tips',
+          children:
+            [
+              {
+                path: 'tips',
+                loadChildren: '../tips/tips.module#TipsPageModule'
+              }
+            ]
+        },
+        {
+          path: '',
+          redirectTo: 'news',
+          pathMatch: 'full'
+        }
+      ]
+  },
+  {
+    path: '',
+    redirectTo: 'news',
+    pathMatch: 'full'
+  }*/
 ];
 
 @NgModule({
